af0284db4640e150862bf5abe873779affc3a0f7
Jeferson <jeferson.aeletrica@gmail.com>
2026-08-08 20:52:41 -0300

    correção status

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ async function getBlingConnectionStatus(connection) {
 
     try {
         // Tenta uma chamada leve na API para validar o token
-        await axios.get('https://www.bling.com.br/Api/v3/usuarios', {
+        await axios.get('https://api.bling.com.br/Api/v3/usuarios', {
             headers: { 'Authorization': `Bearer ${connection.credentials.access_token}` }
         });
         return 'connected';
