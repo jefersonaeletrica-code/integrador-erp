941cc51d8014d73c3f3c0dc5de453ca2b8d6f90a
Jeferson <jeferson.aeletrica@gmail.com>
2026-08-16 22:27:51 -0300

    c

--- dismatal.scraper.js
+++ dismatal.scraper.js
@@ -83,19 +83,9 @@ export class DismatalScraper {
                 retryDelayMs: 2000,
             });
             browserInstance = authResult.browserInstance;
-            const { browser, page: authPage } = browserInstance; // Renomeia a página de autenticação
+            const { page } = browserInstance; // Usaremos a página retornada diretamente
 
             this.logger.info('[DismatalScraper] Autenticação concluída.');
-
-            // Fecha a página de autenticação para evitar instabilidade e scripts em segundo plano.
-            this.logger.info('[DismatalScraper] Fechando página de autenticação...');
-            await authPage.close();
-
-            // Adiciona uma pequena pausa para garantir que o navegador se estabilize após fechar a página.
-            await new Promise(resolve => setTimeout(resolve, 500));
-
-            this.logger.info('[DismatalScraper] Criando nova página para a busca de produtos...');
-            const page = await browser.newPage();
 
             let produtos = [];
 
