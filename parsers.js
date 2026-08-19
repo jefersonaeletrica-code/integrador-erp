e3aca6b328a4ff480916ece64af2841aa6223716
Jeferson <jeferson.aeletrica@gmail.com>
2026-08-19 20:25:26 -0300

    q

--- parsers.js
+++ parsers.js
@@ -37,8 +37,8 @@ export async function isProductPageValid(page) {
     // A verificação deve ser consistente com os seletores de extração.
     // Usamos os seletores de nome e preço para validar a estrutura.
     const hasProductStructure = await page.evaluate((selectors) =>
-        !!(document.querySelector(selectors.productName.join(',')) && document.querySelector(selectors.productPrice.join(','))),
-        { productName: ['div.product-details-container span.title-product', 'h1.product-name'], productPrice: ['div.price-group span.price-group__unity-price', '.price'] }
+        !!(document.querySelector(selectors.productName.join(',')) && document.querySelector(selectors.productPrice.join(','))), {
+            productName: ['div.product-description span.title-product', 'h1.product-name'], productPrice: ['div.price-group span', '.price'] }
     ).catch(() => false);
 
     return hasProductStructure;
