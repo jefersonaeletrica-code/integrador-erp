fd75a2dd7c0ed6a85ea0edd2bbf3e6db51416b13
Jeferson <jeferson.aeletrica@gmail.com>
2026-08-22 20:43:19 -0300

    i

--- db.js
+++ db.js
@@ -1,26 +1,25 @@
 import 'dotenv/config';
 
 const DB_DRIVER = process.env.DB_DRIVER || 'json';
+let dbModule;
 
 const db = {
   _initialized: false,
   
   async initialize() {
     if (this._initialized) return;
-
-    let dbModule;
+    
+    console.log(`Usando driver de banco de dados: ${DB_DRIVER.toUpperCase()}`);
     if (DB_DRIVER === 'json') {
-      console.log('Usando driver de banco de dados: JSON');
       dbModule = await import('./db.json.js');
     } else if (DB_DRIVER === 'mysql') {
-      console.log('Usando driver de banco de dados: MySQL');
       dbModule = await import('./db.mysql.js');
     } else if (DB_DRIVER === 'mongodb') {
-      console.log('Usando driver de banco de dados: MongoDB');
       dbModule = await import('./db.mongodb.js');
     } else {
       throw new Error(`Driver de banco de dados desconhecido: ${DB_DRIVER}`);
     }
+    // Mescla os métodos do módulo de DB específico (ex: getPool, readDb) no objeto 'db'
     Object.assign(this, dbModule);
     this._initialized = true;
   }
