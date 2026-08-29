dd65086fd52b28839b6ce92245e9ed5ff58669f5
Jeferson <jeferson.aeletrica@gmail.com>
2026-08-29 11:42:47 -0300

    l

--- erpService.js
+++ erpService.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import https from 'https';
+import { getLogger } from './logger.js';
 
 // Cria uma instância do axios com configurações reutilizáveis
 const axiosInstance = axios.create({
